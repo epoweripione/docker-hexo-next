935a5cb00d5a35a48355d232d3a4416a3835befc
siyuwuxin <siyuwuxin@gmail.com>
2019-12-28 14:36:13 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -3,17 +3,19 @@ set -ex
 
 POSTS_PATH='/opt/hexo/source/_posts'
 
-echo "Start generation and deployment"
-cd $POSTS_PATH
+if [[ -d "$POSTS_PATH/.git" ]]; then
+    echo "Start generation and deployment"
+    cd "$POSTS_PATH"
 
-echo "Pulling source code..."
-git pull origin master
+    echo "Pulling source code..."
+    git pull origin master
+fi
 
 echo "Generate and deploy..."
 cd /opt/hexo
 hexo clean && hexo g
 
-if [[ ! -z $GULP_MINIFY ]]; then
+if [[ -n "$GULP_MINIFY" ]]; then
     if [[ -d "/opt/hexo/public" && -f "/opt/hexo/gulpfile.js" ]]; then
         echo "gulp minify..."
         gulp
